0a1a46e5565d685b002690affab611caf6761004
kavya-yaddanapudi <kavyayaddanapudi38@gmail.com>
2025-06-28 16:42:33 +0530

    Create Authentication.jsx

---
+++ client/src/pages/Authentication.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Authentication = () => {
+  return (
+    <div>
+      <h2>Authentication Page</h2>
+      {/* Add login/register components or logic here */}
+    </div>
+  );
+};
+
+export default Authentication;
