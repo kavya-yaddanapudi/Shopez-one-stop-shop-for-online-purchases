40d3b855ebfcb4b4b689fb68f092eb816e8df5ed
kavya-yaddanapudi <kavyayaddanapudi38@gmail.com>
2025-06-28 16:19:55 +0530

    Create Login.jsx

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Login logic here
+    console.log(credentials);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={credentials.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={credentials.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
